3c233c52305070f293e389545250e9ae7f365f9f
jesslee2023 <jingping.li@hyperisland.se>
2024-05-23 00:47:19 +0200

    add backgroud pic

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var config = {
     default: "arcade",
     arcade: {
       gravity: { y: 0 },
-      debug: false, // Ensure debug mode is off
+      debug: false,
     },
   },
   scene: {
@@ -14,7 +14,7 @@ var config = {
     create: create,
     update: update,
   },
-  backgroundColor: "#808080", // Set the background color to grey
+  backgroundColor: "#808080",
 };
 
 var player;
@@ -22,9 +22,10 @@ var cursors;
 var healthText;
 var points;
 var game = new Phaser.Game(config);
-var health = 10; // Initialize health variable
+var health = 10;
 
 function preload() {
+  this.load.image("background", "/staff_1024.jpg!w1024_new_0");
   this.load.image("player", "/player.png");
   this.load.image("smoke", "/smoke.png");
   this.load.image("radiation", "/radiation.png");
@@ -33,6 +34,10 @@ function preload() {
 }
 
 function create() {
+  this.background = this.add
+    .tileSprite(0, 0, 800, 600, "background")
+    .setOrigin(0, 0);
+
   player = this.physics.add
     .sprite(40, 300, "player")
     .setScale(0.2)
@@ -76,16 +81,20 @@ function update() {
 
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
-    player.flipX = true; // Flip player to face left
+    this.background.tilePositionX -= 2;
+    player.flipX = true;
   } else if (cursors.right.isDown) {
     player.setVelocityX(160);
-    player.flipX = false; // Reset flip to face right
+    this.background.tilePositionX += 2;
+    player.flipX = false;
   }
 
   if (cursors.up.isDown) {
     player.setVelocityY(-160);
+    this.background.tilePositionY -= 2;
   } else if (cursors.down.isDown) {
     player.setVelocityY(160);
+    this.background.tilePositionY += 2;
   }
 }
 
@@ -93,7 +102,7 @@ function revealItem(player, smoke) {
   var hiddenItem = smoke.getData("hidden");
   if (hiddenItem) {
     hiddenItem.setVisible(true);
-    smoke.destroy(); // Destroy the smoke
+    smoke.destroy();
     this.physics.world.enable(hiddenItem);
     this.physics.add.overlap(
       player,
